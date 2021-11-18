7781b0b3d927857d59c623fb0018d10bb0452455
Jeriel Benavides <jeriel.benavides@dcmail.ca>
2021-11-18 03:10:56 -0500

    Login Page

--- src/App.vue
+++ src/App.vue
@@ -40,7 +40,7 @@ export default {
   src: local("Oswald-Bold"),   url(./fonts/Oswald-Bold.ttf) format("truetype");
 }
 
-* { margin:0; padding:0; box-sizing:border-box; }
+* { margin:0; padding:0; box-sizing:border-box; max-width: 100vw; max-height: 100vh; overflow:hidden}
 
 html{
     scroll-behavior: smooth;  
@@ -53,11 +53,21 @@ html{
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
 }
-
+.exaggerated_padding{
+    padding: 5em !important;
+}
+.btn{
+  background-color: #0B8261 !important;
+}
 h1{
   font-family: Oswald-Bold;
 }
 h5{
   font-family: Oswald-Bold;
+}
+@media only screen and (min-width: 993px){
+.container {
+    width: 90% !important;
+}
 }
 </style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,20 +1,25 @@
 <template>
-    <div class="container row">
-        <div class="col s12 m6 l4">
+    <div class="row container">
+        <div class="login_form col s12 m6 l3">
             <h5>EQUIPMENT SIGN OUT</h5>
             <p></p>
             <p id="login_errors"></p>
-        <form class="">
-                <div class="input-field">
-                    <input id="studentId" type="text" class="validate">
-                    <label for="studentId">Student ID</label>
-                </div>
-                <div class="input-field">
-                    <input id="password" password="Password" type="password" class="validate">
-                    <label for="password">Password</label>
-                </div>
-                <button class="btn waves-effect waves-light" type="submit" name="action">Sign In</button>
-        </form>
+            <form class="">
+                    <div class="input-field">
+                        <input id="studentId" type="text" class="validate">
+                        <label for="studentId">Student ID</label>
+                    </div>
+                    <div class="input-field">
+                        <input id="password" password="Password" type="password" class="validate">
+                        <label for="password">Password</label>
+                    </div>
+                    <button class="btn waves-effect waves-light" type="submit" name="action">Sign In</button>
+            </form>
+        </div>
+        <div class="col s0 m6 l9">
+            <div id="login_img">
+                <img src="../assets/logo_wall.jpg">
+            </div>
         </div>
     </div>
 </template>
@@ -34,5 +39,22 @@ export default {
 <style scoped>
 .input-field input{
     /* border: 1px solid !important; */
+}
+.exaggerated_padding{
+    padding: 5em !important;
+}
+.login_form{
+    padding: 100px;
+}
+#login_img{
+    position: absolute;
+    width: 80vw;
+    padding-left: 10vw;
+}
+#login_img img{
+    width: 100%;
+    min-height: 100vh;
+    /* width: calc(100% + 11vw);
+    height: 100vh; */
 }
 </style>
